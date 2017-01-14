f10f755c1c036e6a8c9df972b274a487adf915d0
volkmaster <ziga.vucko@gmail.com>
2017-01-14 19:52:02 +0100

    moved SVGElement class to timer-progress.js

--- timer-progress.js
+++ timer-progress.js
@@ -13,18 +13,6 @@
  * @constructor
  */
 function TimerProgress (options) {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'svg-element.js');
-    request.onreadystatechange = function () {
-        if (request.status == 200 && request.readyState == 4) {
-            eval(request.responseText);
-            this.setElementProperties(options);
-        }
-    };
-    request.send();
-}
-
-TimerProgress.prototype.setElementProperties = function (options) {
     this.container = new SVGElement(options['container'], {
         'style': 'background-color: ' + options['color-container'] || 'white'
     });
@@ -115,4 +103,71 @@ TimerProgress.prototype.run = function (time, displayText, nDecimals) {
         }
         context.text.element().textContent = (time * (deg / 360) / 1000).toFixed(nDecimals);
     }
+};
+
+/**
+ * Helper class for creating and manipulating SVG elements.
+ * @param {string|Object} element           SVG element type (string) or an existing SVG element (object)
+ * @param {!Object.<string, any>} options   an object containing attributes of the SVG element
+ * @constructor
+ */
+function SVGElement(element, options) {
+    if (typeof(element) === 'string')
+        this.create(element);
+    else if (typeof(element) === 'object')
+        this.wrap(element);
+    this.setBulk(options || null);
+}
+
+SVGElement.prototype.create = function (element) {
+    this.elem = document.createElementNS("http://www.w3.org/2000/svg", element);
+};
+
+SVGElement.prototype.wrap = function (element) {
+    this.elem = element;
+};
+
+SVGElement.prototype.get = function (key) {
+    return this.elem.getAttributeNS(null, key);
+};
+
+SVGElement.prototype.set = function (key, val) {
+    this.elem.setAttributeNS(null, key, val);
+};
+
+SVGElement.prototype.setBulk = function (options) {
+    if (options !== null) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key))
+                this.set(key, options[key]);
+        }
+    }
+};
+
+SVGElement.prototype.addClass = function (cls) {
+    this.elem.classList.add(cls);
+};
+
+SVGElement.prototype.removeClass = function (cls) {
+    this.elem.classList.remove(cls);
+};
+
+SVGElement.prototype.addEventListener = function (type, func) {
+    this.elem.addEventListener(type, func);
+};
+
+SVGElement.prototype.removeEventListener = function (type, func) {
+    this.elem.removeEventListener(type, func);
+};
+
+SVGElement.prototype.appendChild = function (child) {
+    this.elem.appendChild(child.element());
+};
+
+SVGElement.prototype.removeChild = function (child) {
+    this.elem.removeChild(child.element());
+};
+
+SVGElement.prototype.element = function () {
+    return this.elem;
 };
